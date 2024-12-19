534cd68120530adcd9ddf6060de0d381dde97bd1
yylee2002-citi <yaoyulee2002@citi.sinica.edu.tw>
2024-12-19 17:28:56 +0800

    updata

--- bode_f.m
+++ bode_f.m
@@ -1,8 +1,9 @@
 function [mag, phase, freq] = bode_f(sys, f_low, f_high)
     [mag_temp, phase_temp, omega] = bode(sys, {2*pi*f_low, 2*pi*f_high});
     mag = 20 .* log(squeeze(mag_temp));
-    phase_max = ceil(max(phase_temp)/360);
-    phase = squeeze(phase_temp) - phase_max;
+    % phase_max = ceil(max(phase_temp)/360);
+    % phase = squeeze(phase_temp) - phase_max;
+    phase = mod(squeeze(phase_temp), 360) - 360;
     freq = omega ./ (2*pi);
 end
 
